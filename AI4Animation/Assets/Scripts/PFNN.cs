080eb05e82fc30392c828f84af04d4d11627a043
Sebastian Starke <sebastian.starke@ed.ac.uk>
2017-07-28 18:31:34 +0100

    update

--- AI4Animation/Assets/Scripts/PFNN.cs
+++ AI4Animation/Assets/Scripts/PFNN.cs
@@ -26,6 +26,8 @@ public class PFNN {
 	private const float M_PI = 3.14159265358979323846f;
 
 	public PFNN(MODE mode) {
+		Mode = mode;
+		
 		Xp = Matrix<float>.Build.Dense(1, XDim);
 		Yp = Matrix<float>.Build.Dense(1, YDim);
 
@@ -42,10 +44,10 @@ public class PFNN {
 	}
 
 	public void Load() {
-		LoadWeights(Xmean, 1, XDim, "../PFNN/demo/network/pfnn/Xmean.bin");
-		LoadWeights(Xstd,  1, XDim, "../PFNN/demo/network/pfnn/Xstd.bin");
-		LoadWeights(Ymean, 1, YDim, "../PFNN/demo/network/pfnn/Ymean.bin");
-		LoadWeights(Ystd,  1, YDim, "../PFNN/demo/network/pfnn/Ystd.bin");
+		LoadWeights(ref Xmean, 1, XDim, "../PFNN/demo/network/pfnn/Xmean.bin");
+		LoadWeights(ref Xstd,  1, XDim, "../PFNN/demo/network/pfnn/Xstd.bin");
+		LoadWeights(ref Ymean, 1, YDim, "../PFNN/demo/network/pfnn/Ymean.bin");
+		LoadWeights(ref Ystd,  1, YDim, "../PFNN/demo/network/pfnn/Ystd.bin");
     
 		switch(Mode) {
 			case MODE.CONSTANT:
@@ -56,12 +58,12 @@ public class PFNN {
 			b1 = new Matrix<float>[50];
 			b2 = new Matrix<float>[50];
 			for(int i=0; i<50; i++) {
-				LoadWeights(W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+i.ToString("D3")+".bin");
-				LoadWeights(W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+i.ToString("D3")+".bin");
-				LoadWeights(W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+i.ToString("D3")+".bin");
-				LoadWeights(b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+i.ToString("D3")+".bin");
-				LoadWeights(b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+i.ToString("D3")+".bin");
-				LoadWeights(b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+i.ToString("D3")+".bin");
+				LoadWeights(ref W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+i.ToString("D3")+".bin");
+				LoadWeights(ref W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+i.ToString("D3")+".bin");
+				LoadWeights(ref W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+i.ToString("D3")+".bin");
+				LoadWeights(ref b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+i.ToString("D3")+".bin");
+				LoadWeights(ref b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+i.ToString("D3")+".bin");
+				LoadWeights(ref b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+i.ToString("D3")+".bin");
 			}	
 			break;
 			
@@ -73,12 +75,12 @@ public class PFNN {
 			b1 = new Matrix<float>[10];
 			b2 = new Matrix<float>[10];
 			for(int i=0; i<10; i++) {
-				LoadWeights(W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+(i*5).ToString("D3")+".bin");
-				LoadWeights(W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+(i*5).ToString("D3")+".bin");
-				LoadWeights(W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+(i*5).ToString("D3")+".bin");
-				LoadWeights(b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+(i*5).ToString("D3")+".bin");
-				LoadWeights(b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+(i*5).ToString("D3")+".bin");
-				LoadWeights(b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+(i*5).ToString("D3")+".bin");
+				LoadWeights(ref b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+(i*5).ToString("D3")+".bin");
 			}	
 			break;
 
@@ -90,18 +92,18 @@ public class PFNN {
 			b1 = new Matrix<float>[4];
 			b2 = new Matrix<float>[4];
 			for(int i=0; i<4; i++) {
-				LoadWeights(W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+(i*12.5).ToString("D3")+".bin");
-				LoadWeights(W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+(i*12.5).ToString("D3")+".bin");
-				LoadWeights(W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+(i*12.5).ToString("D3")+".bin");
-				LoadWeights(b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+(i*12.5).ToString("D3")+".bin");
-				LoadWeights(b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+(i*12.5).ToString("D3")+".bin");
-				LoadWeights(b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref W0[i], HDim, XDim, "../PFNN/demo/network/pfnn/W0_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref W1[i], HDim, HDim, "../PFNN/demo/network/pfnn/W1_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref W2[i], YDim, HDim, "../PFNN/demo/network/pfnn/W2_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref b0[i], 1, HDim, "../PFNN/demo/network/pfnn/b0_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref b1[i], 1, HDim, "../PFNN/demo/network/pfnn/b1_"+(i*12.5).ToString("D3")+".bin");
+				LoadWeights(ref b2[i], 1, YDim, "../PFNN/demo/network/pfnn/b2_"+(i*12.5).ToString("D3")+".bin");
 			}	
 			break;
 		}
 	}
 
-	private void LoadWeights(Matrix<float> m, int rows, int cols, string fn) {
+	private void LoadWeights(ref Matrix<float> m, int rows, int cols, string fn) {
 		try {
 			BinaryReader reader = new BinaryReader(File.Open(fn, FileMode.Open));
 			m = Matrix<float>.Build.Dense(rows, cols);
@@ -117,7 +119,7 @@ public class PFNN {
         }
 	}
 	
-	public void ELU(Matrix<float> m) {
+	public void ELU(ref Matrix<float> m) {
 		for(int x=0; x<m.RowCount; x++) {
 			for(int y=0; y<m.ColumnCount; y++) {
 				m[x,y] = System.Math.Max(m[x,y], 0f) + (float)System.Math.Exp(System.Math.Min(m[x,y], 0f)) - 1f;
@@ -125,11 +127,11 @@ public class PFNN {
 		}
 	}
 
-	public void Linear(Matrix<float> o, Matrix<float> y0, Matrix<float> y1, float mu) {
+	public void Linear(ref Matrix<float> o, ref Matrix<float> y0, ref Matrix<float> y1, float mu) {
 		o = (1.0f-mu) * y0 + (mu) * y1;
 	}
 
-	public void Cubic(Matrix<float> o, Matrix<float> y0, Matrix<float> y1, Matrix<float> y2, Matrix<float> y3, float mu) {
+	public void Cubic(ref Matrix<float> o, ref Matrix<float> y0, ref Matrix<float> y1, ref Matrix<float> y2, ref Matrix<float> y3, float mu) {
 		o = (
 		(-0.5f*y0+1.5f*y1-1.5f*y2+0.5f*y3)*mu*mu*mu + 
 		(y0-2.5f*y1+2.0f*y2-0.5f*y3)*mu*mu + 
@@ -147,8 +149,8 @@ public class PFNN {
 		switch(Mode) {
 			case MODE.CONSTANT:
 				pindex_1 = (int)((P / (2*M_PI)) * 50);
-				H0 = (W0[pindex_1] * Xp) + b0[pindex_1]; ELU(H0);
-				H1 = (W1[pindex_1] * H0) + b1[pindex_1]; ELU(H1);
+				H0 = (W0[pindex_1] * Xp) + b0[pindex_1]; ELU(ref H0);
+				H1 = (W1[pindex_1] * H0) + b1[pindex_1]; ELU(ref H1);
 				Yp = (W2[pindex_1] * H1) + b2[pindex_1];
 			break;
 			
@@ -157,14 +159,14 @@ public class PFNN {
 				pamount = Mathf.Repeat((P / (2*M_PI)) * 10, 1.0f);
 				pindex_1 = (int)((P / (2*M_PI)) * 10);
 				pindex_2 = ((pindex_1+1) % 10);
-				Linear(W0p, W0[pindex_1], W0[pindex_2], pamount);
-				Linear(W1p, W1[pindex_1], W1[pindex_2], pamount);
-				Linear(W2p, W2[pindex_1], W2[pindex_2], pamount);
-				Linear(b0p, b0[pindex_1], b0[pindex_2], pamount);
-				Linear(b1p, b1[pindex_1], b1[pindex_2], pamount);
-				Linear(b2p, b2[pindex_1], b2[pindex_2], pamount);
-				H0 = (W0p * Xp) + b0p; ELU(H0);
-				H1 = (W1p * H0) + b1p; ELU(H1);
+				Linear(ref W0p, ref W0[pindex_1], ref W0[pindex_2], pamount);
+				Linear(ref W1p, ref W1[pindex_1], ref W1[pindex_2], pamount);
+				Linear(ref W2p, ref W2[pindex_1], ref W2[pindex_2], pamount);
+				Linear(ref b0p, ref b0[pindex_1], ref b0[pindex_2], pamount);
+				Linear(ref b1p, ref b1[pindex_1], ref b1[pindex_2], pamount);
+				Linear(ref b2p, ref b2[pindex_1], ref b2[pindex_2], pamount);
+				H0 = (W0p * Xp) + b0p; ELU(ref H0);
+				H1 = (W1p * H0) + b1p; ELU(ref H1);
 				Yp = (W2p * H1) + b2p;
 			break;
 			
@@ -175,14 +177,14 @@ public class PFNN {
 				pindex_0 = ((pindex_1+3) % 4);
 				pindex_2 = ((pindex_1+1) % 4);
 				pindex_3 = ((pindex_1+2) % 4);
-				Cubic(W0p, W0[pindex_0], W0[pindex_1], W0[pindex_2], W0[pindex_3], pamount);
-				Cubic(W1p, W1[pindex_0], W1[pindex_1], W1[pindex_2], W1[pindex_3], pamount);
-				Cubic(W2p, W2[pindex_0], W2[pindex_1], W2[pindex_2], W2[pindex_3], pamount);
-				Cubic(b0p, b0[pindex_0], b0[pindex_1], b0[pindex_2], b0[pindex_3], pamount);
-				Cubic(b1p, b1[pindex_0], b1[pindex_1], b1[pindex_2], b1[pindex_3], pamount);
-				Cubic(b2p, b2[pindex_0], b2[pindex_1], b2[pindex_2], b2[pindex_3], pamount);
-				H0 = (W0p * Xp) + b0p; ELU(H0);
-				H1 = (W1p * H0) + b1p; ELU(H1);
+				Cubic(ref W0p, ref W0[pindex_0], ref W0[pindex_1], ref W0[pindex_2], ref W0[pindex_3], pamount);
+				Cubic(ref W1p, ref W1[pindex_0], ref W1[pindex_1], ref W1[pindex_2], ref W1[pindex_3], pamount);
+				Cubic(ref W2p, ref W2[pindex_0], ref W2[pindex_1], ref W2[pindex_2], ref W2[pindex_3], pamount);
+				Cubic(ref b0p, ref b0[pindex_0], ref b0[pindex_1], ref b0[pindex_2], ref b0[pindex_3], pamount);
+				Cubic(ref b1p, ref b1[pindex_0], ref b1[pindex_1], ref b1[pindex_2], ref b1[pindex_3], pamount);
+				Cubic(ref b2p, ref b2[pindex_0], ref b2[pindex_1], ref b2[pindex_2], ref b2[pindex_3], pamount);
+				H0 = (W0p * Xp) + b0p; ELU(ref H0);
+				H1 = (W1p * H0) + b1p; ELU(ref H1);
 				Yp = (W2p * H1) + b2p;
 			break;
 			
