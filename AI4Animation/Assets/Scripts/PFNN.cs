b165b0412877fbb639e28dbb138249372f215e2d
Sebastian Starke <starke@informatik.uni-hamburg.de>
2017-07-28 12:45:48 +0100

    update

--- AI4Animation/Assets/Scripts/PFNN.cs
+++ AI4Animation/Assets/Scripts/PFNN.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
 
-public class PFNN {
+public class PFNN : MonoBehaviour {
 
-	
+	void Update() {
+		Matrix<double> m = Matrix<double>.Build.Random(3, 4);
+		Vector<double> v = Vector<double>.Build.Random(4);
+		Vector<double> result = m*v;
+		Debug.Log(result);
+	}
 
 }
