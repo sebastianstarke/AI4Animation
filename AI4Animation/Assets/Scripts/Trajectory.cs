a6ccfb9f1fcace86b2b6542341220085b88bb9e7
Sebastian Starke <sebastian.starke@ed.ac.uk>
2017-07-29 21:15:09 +0100

    update

--- AI4Animation/Assets/Scripts/Trajectory.cs
+++ AI4Animation/Assets/Scripts/Trajectory.cs
@@ -2,6 +2,40 @@
 
 public class Trajectory {
 
-	
+	public int Length = 120;
+
+	public float Width = 25f;
+
+	public Vector3[] Positions;
+	public Vector3[] Directions;
+	public Quaternion[] Rotations;
+	public float[] Heights;
+
+	public float[] GaitStand;
+	public float[] GaitWalk;
+	public float[] GaitJog;
+	public float[] GaitCrouch;
+	public float[] GaitJump;
+	public float[] GaitBump;
+  
+  	public Vector3 TargetDirection;
+	public Vector3 TargetVelocity;
+
+	public Trajectory() {
+		Positions = new Vector3[Length];
+		Directions = new Vector3[Length];
+		Rotations = new Quaternion[Length];
+		Heights = new float[Length];
+
+		GaitStand = new float[Length];
+		GaitWalk = new float[Length];
+		GaitJog = new float[Length];
+		GaitCrouch = new float[Length];
+		GaitJump = new float[Length];
+		GaitBump = new float[Length];
+
+		TargetDirection = new Vector3(0f,0f,1f);
+		TargetVelocity = new Vector3(0f,0f,0f);
+	}
 
 }
