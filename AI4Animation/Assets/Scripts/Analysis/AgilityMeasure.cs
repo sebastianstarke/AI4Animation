aa975b7b498ead980e3a7b71c87ba6c04d6b433b
Sebastian Starke <Sebastian.Starke@ed.ac.uk>
2018-04-26 16:49:17 +0100

    update

--- AI4Animation/Assets/Scripts/Analysis/AgilityMeasure.cs
+++ AI4Animation/Assets/Scripts/Analysis/AgilityMeasure.cs
@@ -9,30 +9,33 @@ public class AgilityMeasure : MonoBehaviour {
 	public Transform[] Bones;
 
 	private Quaternion[] PreviousRotations;
-	private List<float> Angles;
+	private List<float> Values;
 
 	void Start() {
 		PreviousRotations = new Quaternion[Bones.Length];
 		for(int i=0; i<PreviousRotations.Length; i++) {
 			PreviousRotations[i] = Bones[i].localRotation;
 		}
-		Angles = new List<float>();
+		Values = new List<float>();
 	}
 
 	void LateUpdate () {
+		float value = 0f;
 		for(int i=0; i<Bones.Length; i++) {
 			Quaternion rotation = Bones[i].localRotation;
-			Angles.Add(Quaternion.Angle(PreviousRotations[i], rotation));
+			value += Quaternion.Angle(PreviousRotations[i], rotation);
 			PreviousRotations[i] = rotation;
 		}
+		value /= Bones.Length;
+		Values.Add(value);
 	}
 
 	void OnGUI() {
 		GUI.color = UltiDraw.Mustard;
 		GUI.backgroundColor = UltiDraw.Black;
 		if(GUI.Button(Utility.GetGUIRect(0.005f, Position, 0.02f, 0.02f), "X")) {
-			Angles.Clear();
+			Values.Clear();
 		};
-		GUI.Box(Utility.GetGUIRect(0.025f, Position, 0.175f, 0.02f), "Average Agility: " + Utility.ComputeMean(Angles.ToArray()));
+		GUI.Box(Utility.GetGUIRect(0.025f, Position, 0.175f, 0.02f), "Average Agility: " + Utility.ComputeMean(Values.ToArray()));
 	}
 }
