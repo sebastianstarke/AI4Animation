38b5ee6cf47b548f0cd8bb0755fa77429bc494bf
Sebastian Starke <Sebastian.Starke@ed.ac.uk>
2018-06-07 23:33:12 -0700

    update

--- AI4Animation/Assets/Palette/Water/Water/Scripts/GerstnerDisplace.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-namespace UnityStandardAssets.Water
-{
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(WaterBase))]
-    public class GerstnerDisplace : Displace { }
-}
