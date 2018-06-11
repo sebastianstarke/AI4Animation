fd49d459178cec6e7952dc34d414e0ca419a416b
Sebastian Starke <Sebastian.Starke@ed.ac.uk>
2018-06-11 04:58:38 -0700

    update

--- AI4Animation/Assets/Scripts/DataProcessing/FBXImporter.cs
+++ AI4Animation/Assets/Scripts/DataProcessing/FBXImporter.cs
@@ -174,47 +174,47 @@ public class FBXImporter : EditorWindow {
 			Importing = true;
 			for(int f=0; f<Files.Length; f++) {
 				if(Files[f].Import) {
-					AnimationClip clip = (AnimationClip)AssetDatabase.LoadAssetAtPath(AssetDatabase.GetAssetPath(Files[f].Object), typeof(AnimationClip));
-					MotionData data = ScriptableObject.CreateInstance<MotionData>();
-					data.Name = Files[f].Object.name.Substring(Files[f].Object.name.LastIndexOf("/")+1);
-					if(AssetDatabase.LoadAssetAtPath(destination+"/"+data.Name+".asset", typeof(MotionData)) == null) {
+					string name = Files[f].Object.name.Substring(Files[f].Object.name.LastIndexOf("/")+1);
+					if(AssetDatabase.LoadAssetAtPath(destination+"/"+name+".asset", typeof(MotionData)) == null) {
+						AnimationClip clip = (AnimationClip)AssetDatabase.LoadAssetAtPath(AssetDatabase.GetAssetPath(Files[f].Object), typeof(AnimationClip));
+						MotionData data = ScriptableObject.CreateInstance<MotionData>();
+
+						//Assign Name
+						data.Name = name;
+
 						AssetDatabase.CreateAsset(data , destination+"/"+data.Name+".asset");
+
+						//Create Source Data
+						data.Source = new MotionData.Hierarchy();
+						for(int i=0; i<Character.Bones.Length; i++) {
+							data.Source.AddBone(Character.Bones[i].GetName(), Character.Bones[i].GetParent() == null ? "None" : Character.Bones[i].GetParent().GetName());
+						}
+
+						//Set Frames
+						ArrayExtensions.Resize(ref data.Frames, Mathf.RoundToInt((float)Framerate * clip.length));
+
+						//Set Framerate
+						data.Framerate = (float)Framerate;
+
+						//Compute Frames
+						for(int i=0; i<data.GetTotalFrames(); i++) {
+							data.Frames[i] = new MotionData.Frame(data, i+1, (float)i / data.Framerate);
+							clip.SampleAnimation(Character.gameObject, data.Frames[i].Timestamp);
+							for(int j=0; j<Character.Bones.Length; j++) {
+								data.Frames[i].Local[j] = Character.Bones[j].Transform.GetLocalMatrix();
+								data.Frames[i].World[j] = Character.Bones[j].Transform.GetWorldMatrix();
+							}
+						}
+
+						//Finalise
+						data.DetectHeightMapSensor();
+						data.DetectDepthMapSensor();
+						data.DetectSymmetry();
+						data.ComputeStyles();
+						data.AddSequence();
 					} else {
-						int i = 1;
-						while(AssetDatabase.LoadAssetAtPath(destination+"/"+data.Name+data.Name+" ("+i+").asset", typeof(MotionData)) != null) {
-							i += 1;
-						}
-						AssetDatabase.CreateAsset(data, destination+"/"+data.Name+data.Name+" ("+i+").asset");
+						Debug.Log("File with name " + name + " already exists.");
 					}
-
-					//Create Source Data
-					data.Source = new MotionData.Hierarchy();
-					for(int i=0; i<Character.Bones.Length; i++) {
-						data.Source.AddBone(Character.Bones[i].GetName(), Character.Bones[i].GetParent() == null ? "None" : Character.Bones[i].GetParent().GetName());
-					}
-
-					//Set Frames
-					ArrayExtensions.Resize(ref data.Frames, Mathf.RoundToInt((float)Framerate * clip.length));
-
-					//Set Framerate
-					data.Framerate = (float)Framerate;
-
-					//Compute Frames
-					for(int i=0; i<data.GetTotalFrames(); i++) {
-						data.Frames[i] = new MotionData.Frame(data, i+1, (float)i / data.Framerate);
-						clip.SampleAnimation(Character.gameObject, data.Frames[i].Timestamp);
-						for(int j=0; j<Character.Bones.Length; j++) {
-							data.Frames[i].Local[j] = Character.Bones[j].Transform.GetLocalMatrix();
-							data.Frames[i].World[j] = Character.Bones[j].Transform.GetWorldMatrix();
-						}
-					}
-
-					//Finalise
-					data.DetectHeightMapSensor();
-					data.DetectDepthMapSensor();
-					data.DetectSymmetry();
-					data.ComputeStyles();
-					data.AddSequence();
 				}
 			}
 			Importing = false;
